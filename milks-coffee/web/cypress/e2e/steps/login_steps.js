9e3e567a53f558c2511ed31388ed6100efdce186
Stefany Francine Pasold <stefanyfrancine@gmail.com>
2026-05-09 16:06:42 -0300

    correção no arquivo de login_steps

--- milks-coffee/web/cypress/e2e/steps/login_steps.js
+++ milks-coffee/web/cypress/e2e/steps/login_steps.js
@@ -2,10 +2,15 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import LoginPage from "../../support/pages/LoginPage.js";
 
 Given("que eu acesso a página de login", () => {
+    cy.log('#### Acessando a URL base ####');
     LoginPage.acessar();
 });
 
 When("eu insiro o usuário {string} e a senha {string}", (usuario, senha) => {
+    // Interceptamos a chamada de login para validar o status code depois
+    cy.intercept('POST', '**/login').as('postLogin');
+    
+    cy.log(`#### Tentando login com: ${usuario} ####`);
     LoginPage.preencherCredenciais(usuario, senha);
 });
 
@@ -14,28 +19,33 @@ When("clico em entrar", () => {
 });
 
 Then("eu devo ver a mensagem de erro {string}", (mensagem) => {
+    // Validação PRO: Espera o erro 401 do servidor antes de checar a UI
+    cy.wait('@postLogin').its('response.statusCode').should('eq', 401);
+    
     LoginPage.validarErro(mensagem);
 });
 
 Then("eu devo ser redirecionado para o painel do PDV", () => {
+    cy.wait('@postLogin').its('response.statusCode').should('eq', 200);
+    
     cy.url().should('include', '/pdv');
-    cy.get('[data-cy="titulo-painel-admin"]').should('be.visible');
+    cy.get('[data-cy="titulo-painel-admin"]').should(($el) => {
+        expect($el).to.be.visible;
+        expect($el.text()).to.match(/Painel|PDV/i); // Flexibilidade no texto
+    });
 });
 
 Then("eu devo ser redirecionado para a vitrine da Loja", () => {
+    cy.wait('@postLogin').its('response.statusCode').should('eq', 200);
+    
     cy.url().should('include', '/loja');
     cy.get('[data-cy="titulo-loja-cliente"]').should('be.visible');
 });
 
-// Passos de Background para reutilização
+// Reutilização com comandos de log para o vídeo
 Given("que estou logado como administrador", () => {
+    cy.log('#### Setup: Logando como Admin ####');
     LoginPage.acessar();
     LoginPage.preencherCredenciais("admin", "123");
-    LoginPage.submeter();
-});
-
-Given("que estou logado como cliente", () => {
-    LoginPage.acessar();
-    LoginPage.preencherCredenciais("stefany", "123456");
     LoginPage.submeter();
 });
