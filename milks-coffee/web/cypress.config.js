2bd80107a85a63b0755ef0ae83bf62942562619a
Stefany Francine Pasold <stefanyfrancine@gmail.com>
2026-05-18 16:38:03 -0300

    chore: prepara arquivo cypress.config.js para receber as configurações de relatório

--- milks-coffee/web/cypress.config.js
+++ milks-coffee/web/cypress.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
     baseUrl: "http://localhost:5173",
     viewportWidth: 1280,
     viewportHeight: 720,
+    video: false,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    setupNodeEvents(on, config) {
+      // Aqui entrarão as configurações para relatórios
+      return config;
+    }
   },
 });
